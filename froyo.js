ab641aec6fbcbf544917e425fe0951c945859d6d
Brian Capraro <Briancapraro0228@gmail.com>
2023-09-21 16:23:24 -0400

    Updated user prompt in JS and changed variable name.

--- froyo.js
+++ froyo.js
@@ -1,1 +1,5 @@
-const froyoFlavors = prompt("Please enter your froyo flavors seperated by commas. Thanks!");
+const userInputString = prompt(
+  "Please enter your froyo flavors seperated by commas. Thanks!",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+
