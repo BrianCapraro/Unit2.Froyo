05134f5da60d718632f9e16b7d2e79d439ba61ce
Brian Capraro <Briancapraro0228@gmail.com>
2023-09-21 19:00:45 -0400

    Created Object and for...of loop that gathers properties and values from the previous array and puts into the created object.

--- froyo.js
+++ froyo.js
@@ -1,10 +1,25 @@
-const userInputString = prompt(
+const userInputFlavors = prompt(
   "Please enter your froyo flavors seperated by commas. Thanks!",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
-const strArray = userInputString.split(",")
+const flavorArray = userInputFlavors.split(",");
 // checking to see if the array now seperates the order
-// console.log(strArray)
+console.log(flavorArray);
 
+//used to initilize an empty object from the array that was submitted upon user input
+const flavorCounts = {};
 
+//use a for...of loop to iterate through the previous input array and count each flavor.
+//when a flavor is encountered for the first time is added as a property with a value of 1
+//every time a property is encountered again its value is in increased by 1.
+for (const flavor of flavorArray) {
+  if (flavorCounts[flavor]) {
+    flavorCounts[flavor]++;
+  } else {
+    flavorCounts[flavor] = 1;
+  }
+}
+
+//just wanted to console the table to display the object, property, and associated values
+console.table(flavorCounts);
