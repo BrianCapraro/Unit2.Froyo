758e59386804bc625a09882ca1415222ee374802
Brian Capraro <Briancapraro0228@gmail.com>
2023-09-21 16:27:29 -0400

    Updated user prompt in JS and changed variable name. Also created array from user input data w/test.

--- froyo.js
+++ froyo.js
@@ -3,3 +3,8 @@ const userInputString = prompt(
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
+const strArray = userInputString.split(",")
+// checking to see if the array now seperates the order
+// console.log(strArray)
+
+
